1dbdea42c42b50ff604497e7aa4c52d0c58b1a09
AlvaroM <alvaro.pinheiro@edu.ufes.br>
2023-10-27 12:05:54 -0300

    melhorias 1

--- src/app/components/views/email/email.component.ts
+++ src/app/components/views/email/email.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { EmailService } from 'src/app/service/email.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email',
@@ -14,6 +15,9 @@ export class EmailComponent {
   assunto: string;
   mensagem: string;
   mensagemExtra: string;
+  msgControl = new FormControl('', [Validators.required]);
+  assControl = new FormControl('', [Validators.required]);
+
 
   constructor(private service: EmailService, private router: Router) { }
 
@@ -21,11 +25,29 @@ export class EmailComponent {
     if(this.selectAssunto != 'Outro'){
       this.assunto = this.selectAssunto;
     }
-    if(this.selectAssunto == 'Orçamento'){
-      this.service.sendEmailOrçamento(this.email, this.assunto, this.mensagem, this.mensagemExtra);
-    }else{
-      this.service.sendEmail(this.email, this.assunto, this.mensagem);
+    if(this.mensagem == null || this.mensagem == '' || this.assunto == null || this.assunto == ''){
+      alert('Você deve preencher todos os campos obrigatórios');
+      return;
     }
+    // if(this.selectAssunto == 'Orçamento'){
+    //   this.service.sendEmailOrçamento(this.email, this.assunto, this.mensagem, this.mensagemExtra);
+    // }else{
+    //   this.service.sendEmail(this.email, this.assunto, this.mensagem);
+    // }
     this.router.navigate(['']);
+  }
+
+  getErrorMessage() {
+    if (this.msgControl.hasError('required')) {
+      return 'Você deve digitar uma mensagem';
+    }
+    return '';
+  }
+
+  getErrorAssunto(){
+    if (this.assControl.hasError('required')) {
+      return 'Você deve adicionar um assunto';
+    }
+    return '';
   }
 }
